ae5119d15bb24615ba32709061538bdfb3dffa66
bwikp <zilpa.michel@gmail.com>
2024-05-07 21:42:43 +0200

    rebuild

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,96 +1,19 @@
 <template>
-    <div class="WelcomeZone">
-        <div class="wtext">Bonjour Je suis</div>
-        <div class="bigText">Michel Zilpa</div>
-        <div class="bigTitre">Développeur Web en formation</div>
-        <div class="introMoi">Je suis actuellement en formation
-et pour obtenir le titre professionnel de développeur web, j'ai besoin
-de trouver un stage de 2 mois et 4 jours du 1er juin au 28 juillet 2023.
-j'espère que l'on pourra travailler ensemble très bientôt.
-
-        <div  class="bigTitre">Si vous êtes intéressé ou curieux d'en savoir plus :</div> 
-            <div class="zoneCv">
-                <h2>Voici Mon cv</h2>
-                <a href="/assets/Michel_Zilpa.pdf"><button class="buttonCV">Mon CV</button></a>
+    <div class="containerHome">
+        <div class="Landing">
+                <p1>Michel Zilpa</p1>
+                <p2>Développeur Web</p2>
+            <div>
+            <p2>En Rercher d'alternance</p2>
             </div>
         </div>
-
+        <div class="landingLink">
+            <a href="https://simplon.co/formation/concepteur-developpeur-d-applications/12" target="_blank"><img class="svgIcon" src="/src/assets/SIMPLON.svg" alt=""></a>
+            <a href="https://github.com/bwikp" target="_blank"><img class="svgIcon" src="/src/assets/github-mark.svg"/></a>
+            <a href="https://github.com/bwikp" target="_blank"><img class="svgIcon" src="/src/assets/linkedin.svg"/></a>
+        </div>
     </div>
 </template>
 <style scoped>
-        
-    .introMoi>.bigTitre{
-        margin-top: 20px;
-    }
-    .zoneCv{
-        margin-top: 25px;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-    }
-.buttonCV {
-    margin-top: 20px ;
-    width: 200px;
-    border-radius: 15px;
-    height: 100px;
-  box-shadow: rgb(179, 62, 29)  0px 0px 5px 5px;
-}
-
-.introMoi {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    font-size: 20px;
-
-}
-
-.bigTitre {
-    font-size: 26px;
-    font-weight: bolder;
-    color: rgb(226, 60, 60);
-}
-
-.wtext {
-    font-size: 20px;
-}
-
-.wtext,.bigText,.bigTitre {
-    padding-bottom: 10px;
-}
-
-.bigText {
-    font-weight: 500;
-    font-size: 40px;
-    font-family: 'Open Sans';
-    color: red;
-}
-
-.WelcomeZone {
-    display: flex;
-    flex-direction: column;
-    align-items: start;
-    justify-content: start;
-    padding-top: 75px;
-    font-family: 'Playfair Display', sans-serif;
-    width: 85%;
-    height: 100%;
-    color: black;
-}
-
-#internship {
-    font-size: 30px;
-}
-
-p {
-    font-size: 26px;
-}
-
-h1 {
-    padding-bottom: 20px;
-    padding-left: 25px;
-    font-weight: 100;
-    font-size: 52px;
-    color: rgb(178, 60, 60);
-}</style>
+    
+</style>
--- src/views/ProjectPage.vue
+++ src/views/ProjectPage.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="gallery">
-        <div class="grids post1"><a href="/projet/horloge/" target="_blank">L'Horloge</a></div>
-        <div class="grids post2"><a href="/projet/MiniProjetJs/" target="_blank">Shifumi</a></div>
-        <div class="grids post3"><a href="/projet/TimeTracker/TimeTracker-Sujet/" target="_blank">TimeTracker</a></div>
-        <div class="grids post4"><a href="assets/Location.png" target="_blank">Bdd Location de voiture </a></div>
-        <div class="grids post5"><a href="/projet/loca-cars/" target="_blank">site location voiture</a></div>
-        <div class="grids post6"><a href="/projet/album" target="_blank">List d'album Vue</a></div>
+        <div class="grids post1"><a href="https://github.com/bwikp/BookReviewSite" target="_blank">DLBook Front</a></div>
+        <div class="grids post2"><a href="https://github.com/bwikp/ApirestSymfony" target="_blank">DLBook User Api</a></div>
+        <div class="grids post3"><a href="#" >Working on it</a></div>
+        <div class="grids post4"><a href="#" >Working on it  </a></div>
+        <div class="grids post5"><a href="#" >Working on it </a></div>
+        <div class="grids post6"><a href="#" >Working on it </a></div>
     </div>
 </template>
 <style scoped>
@@ -31,38 +31,34 @@ a:hover{
     flex-direction: row;
     justify-content: center;
     align-items:center;
-    transition: 2.5s;
+    transition: 0.1s;
     height: 20%;
     width: 100%;
  }
+
+
 .post1:hover{       
     background-size: 100%;
-    transition: 1s;
     background:rgb(224, 179, 179);
 }
 
 .post2:hover{
     background: rgb(159, 156, 231);
-    transition: 1s ease-in;
 }
 
 .post3:hover{
     background: rgb(160, 196, 170);
-    transition: 1s ease-in-out;
 }
 .post4:hover{
     background: rgb(167, 177, 58);
-    transition: 1s ease-in;
 }
 
 
 .post5:hover{
     background: rgb(122, 71, 138);
-    transition: 1s ease-in;
 }
 .post6:hover{
     background: rgb(13, 194, 218);
-    transition: 1s ease-in;
 }
 
 </style>
