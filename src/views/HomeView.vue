17fa83d91c40b6b9a56145b38eabd20cc8709b3e
bwikp <zilpa.michel@gmail.com>
2024-05-12 20:29:26 +0200

    landing page done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,34 +5,49 @@
         <p1 class="Ltitle">Zilpa Michel</p1>
         <p2 class="Ltitle">Développeur Web sur Lyon</p2>
         <p>
-          je suis un jeune Développeur à la recherche d'un Stage pour ma
-          formation
+          je suis un jeune Développeur à la recherche d'une Alternance d'un an
+          pour ma formation
           <b
             ><a
-              href="https://simplon.co/formation/concepteur-developpeur-d-applications/12" target="_blank"
+              href="https://simplon.co/formation/concepteur-developpeur-d-applications/12"
+              target="_blank"
               >Concepteur-Developpeur-d-Applications</a
             >
           </b>
           chez simplon. J'ai commencé mon apprentisage grâce a FreecodeCamp puis
-          j'ai suivi une formation de Développeur Web et j'ai pu obtenir le titre
-          professionel de Développeur web et web mobile.
+          j'ai suivi une formation de Développeur Web et j'ai pu obtenir le
+          titre professionel de Développeur web et web mobile.
         </p>
+        <p>
+          Désireux de toujours en apprendre plus je ferais tout mon possible
+          pour devenir un élément remarquable de vos équipes, grâce à mon
+          travail et notre collaboration.
+        </p>
+        <p>Merci d'avoir pris le temps de lire ma candidature !</p>
       </div>
-      <div class="Zone2">Zone 2</div>
+      <div class="Zone2">
+        <div class="landingLink">
+          <div>
+            <a href="https://simplon.co/" target="_blank"
+              ><img class="svgIcon" src="/src/assets/SIMPLON.svg" alt=""
+            /></a>
+            <a href="https://github.com/bwikp" target="_blank"
+              ><img class="svgIcon" src="/src/assets/github-mark.svg"
+            /></a>
+          </div>
+          <div>
+            <a
+              href="https://www.linkedin.com/in/michel-zilpa-b66a64235/"
+              target="_blank"
+              ><img class="svgIcon" src="/src/assets/linkedin.svg" />
+            </a>
+            <a href="mailto:zilpa.michel@gmail.com"
+              ><img class="svgIcon" src="/src/assets/email-icon.svg"
+            /></a>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-  <div class="landingLink">
-    <a
-      href="https://simplon.co/"
-      target="_blank"
-      ><img class="svgIcon" src="/src/assets/SIMPLON.svg" alt=""
-    /></a>
-    <a href="https://github.com/bwikp" target="_blank"
-      ><img class="svgIcon" src="/src/assets/github-mark.svg"
-    /></a>
-    <a href="https://www.linkedin.com/in/michel-zilpa-b66a64235/" target="_blank"
-      ><img class="svgIcon" src="/src/assets/linkedin.svg"
-    /></a>
   </div>
 </template>
 <style scoped></style>
