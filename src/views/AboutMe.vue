efe372b5ef5d85d3c0279d3d667b56a79012670d
bwikp <zilpa.michel@gmail.com>
2024-05-14 22:37:56 +0200

    css fix

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -1,32 +1,36 @@
 <template>
-<div class="info">
+  <div class="info">
     <div class="infoP">
-         <h2>Zilpa Michel</h2>
-         <h3>27/03/2001</h3>
+      <h2>Zilpa Michel</h2>
+      <h3>27/03/2001</h3>
     </div>
     <div class="stage">
-        <p>Je suis actuellement 
-        </p>
+      <p>Je suis actuellement</p>
     </div>
-    <div class="formation ">
-        <h2>Qui suis je ?</h2>
-        <p class="moi">
-            Je suis un apprenti développeur basé sur Lyon, qui aujourd'hui est plus motivé que jamais.
-            À rejoindre le monde du web. Je suis curieux et avide de savoir, je ne cherche rien d'autre
-            que l'opportunité d'intégrer le monde du web et de l'informatique en général.
-
-        </p>
-    </div>
-        <h2 class="titleFormation">formation</h2>
     <div class="formation">
-        <ul>
-            <li>Formation développeur web :  Titre professionel Développeur web et web mobile</li>
-            <li>FreeCodeCamp :  Responsive Web certifications</li>
-            <li>JavaScript Algorithmes & DataStructure certifications</li>
-            <li>Licence mathématique/informatique Lyon 1 : 2 ans</li>
-            <li>Licence Langue  étrangère appliquée Lyon 3 : anglais chinois 1 an</li>
-            <li>Baccalauréat Scientifique, option science de l'ingénieur.</li>
-        </ul>  
+      <h2>Qui suis je ?</h2>
+      <p class="moi">
+        Je suis un apprenti développeur basé sur Lyon, qui aujourd'hui est plus
+        motivé que jamais. À rejoindre le monde du web. Je suis curieux et avide
+        de savoir, je ne cherche rien d'autre que l'opportunité d'intégrer le
+        monde du web et de l'informatique en général.
+      </p>
     </div>
-</div>
+    <h2 class="titleFormation">formation</h2>
+    <div class="formation">
+      <ul>
+        <li>
+          Formation développeur web : Titre professionel Développeur web et web
+          mobile
+        </li>
+        <li>FreeCodeCamp : Responsive Web certifications</li>
+        <li>JavaScript Algorithmes & DataStructure certifications</li>
+        <li>Licence mathématique/informatique Lyon 1 : 2 ans</li>
+        <li>
+          Licence Langue étrangère appliquée Lyon 3 : anglais chinois 1 an
+        </li>
+        <li>Baccalauréat Scientifique, option science de l'ingénieur.</li>
+      </ul>
+    </div>
+  </div>
 </template>
