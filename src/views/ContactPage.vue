eb18e83e1e0872ef2d64b67821e1d136e5a62f35
bwikp <zilpa.michel@gmail.com>
2024-05-24 23:37:31 +0200

    page contact a finir api mail a faire en GO

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -1,39 +1,86 @@
 <template>
- <div class="pageContact">
-    <div class="topContact">
-    <h1>CONTACT</h1>
-    <hr>
-    </div>
-    <h2>Email</h2>
-    <h3 class="underline"><a href="mailto:zilpa.zichel@gmail.com">zilpa.michel@gmail.com</a></h3>
-    <h2>Réseaux scoiaux</h2>
-     <h3><a class="underline" href="https://github.com/bwikp">github</a></h3>
-     <h3><a class="underline" href="https://www.linkedin.com/in/michel-zilpa-b66a64235/">linkedin</a></h3>
- </div>
-</template>
-<style scoped>
+  <div class="pushingP">
+    <div class="pageContact">
+      <h1>Contact</h1>
+      <div>
+        <textarea name="" id=""></textarea>
+      </div>
+      <div class="sendMail front-end">Envoyer</div>
 
-    hr{
-        color: red;
-        width:  100%;
-    }
-    
-    .topContact{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        width: 100%;
-    }
-    .pageContact{
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items:center;
-        margin-top: 50px;
-        font-family: 'lato',sans-serif;
-        font-size: 26px;
-        gap: 50px;
-        width: 80%;
-        font-family: 'lato',sans-serif;
-    }
+    </div>
+    <div class="ContactList">
+        <h3 class="underline">
+        <img class=" contactIcon" src="/src/assets/email-icon.svg" />
+          <a href="mailto:zilpa.zichel@gmail.com"></a>
+        </h3>     
+        <h3 class="underline"><a href="https://github.com/bwikp">
+        <img class=" contactIcon" src="/src/assets/github-mark.svg" />
+        </a></h3>
+        <h3 class="underline">
+          <a href="https://www.linkedin.com/in/michel-zilpa-b66a64235/" target="_blank">
+        <img class="contactIcon" src="/src/assets/linkedin.svg" />
+          </a>
+        </h3>
+      </div>
+    </div>
+</template>
+<style>
+
+.sendMail{
+    height: 50px;
+    width: 125px;
+    border-radius: 20.5px;
+    border: 1px solid black;
+    text-align: center;
+    align-content: center;
+    font-size: 20px;
+}
+
+.sendMail:hover{
+    cursor: pointer;
+    background: rgba(162, 223, 178, 0.6);
+}
+.pushingP {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+.ContactList {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 15%;
+  gap: 2%;
+}
+
+.underline img {
+    height: 50px;
+    width: 50px;
+  box-shadow: none;
+  padding: 0;
+  border-radius: 0;
+  object-fit: contain;
+  width: 100px;
+}
+
+.contactIcon {
+  height: 50px;
+  height: 50px;
+}
+
+.pageContact {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-family: "lato", sans-serif;
+  width: 80%;
+  height: 85%;
+  font-family: "lato", sans-serif;
+}
 </style>
